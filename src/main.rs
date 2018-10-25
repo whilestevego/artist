6aa21134391f3628c6f4dd3c9c77983598810ea2
Steve Godin <steve.god@gmail.com>
2018-10-24 22:10:49 -0700

    Draw more lines with the turtle

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use image::{ImageBuffer, Rgba, RgbaImage};
 use turtle::*;
 
 fn main() {
-    let mut img: RgbaImage = ImageBuffer::new(255, 255);
+    let mut img: RgbaImage = ImageBuffer::new(100, 100);
 
     for (_x, _y, pixel) in img.enumerate_pixels_mut() {
         *pixel = Rgba([255, 255, 255, 255])
@@ -14,14 +14,16 @@ fn main() {
     let mut turtle = Turtle::default();
 
     turtle
-        .turn(90.0)
-        .forward(10.0)
-        .turn(-90.0)
-        .forward(50.0)
+        .turn(20.0)
+        .forward(70.0)
         .turn(90.0)
         .forward(50.0)
-        .turn(-43.0)
+        .turn(90.0)
         .forward(50.0)
+        .turn(90.0)
+        .forward(50.0);
+
+    turtle
         .points()
         .for_each(|Point { x, y }| img.put_pixel(x as u32, y as u32, Rgba([0, 0, 0, 255])));
 
