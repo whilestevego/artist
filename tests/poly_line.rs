7d7e858c413759fe063be696c3074e47d7ed3193
Steve Godin <steve.god@gmail.com>
2018-10-28 12:57:56 -0700

    Add more tests for PolyLine

--- tests/poly_line.rs
+++ tests/poly_line.rs
@@ -2,6 +2,15 @@ extern crate turtle;
 
 use turtle::*;
 
+// Cartesian Quadrants
+//         |
+//    2    |    1
+//         |
+// -----------------
+//         |
+//    3    |    4
+//         |
+
 #[test]
 fn it_draws_two_lines_at_90_deg() {
     let poly_line = PolyLine::new(vec![(0.0, 0.0), (3.0, 3.0), (0.0, 3.0)]);
@@ -22,9 +31,57 @@ fn it_draws_two_lines_at_90_deg() {
 }
 
 #[test]
-fn it_draws_stuff() {
-    let poly_line = PolyLine::new(vec![(6.0, 0.0), (3.0, 10.0), (0.0, 0.0)]);
-    let points = poly_line.plot().collect::<Vec<_>>();
+fn it_draws_a_square_in_first_quadrant() {
+    let poly_line = PolyLine::new(vec![
+        (0.0, 0.0),
+        (3.0, 0.0),
+        (3.0, 3.0),
+        (0.0, 3.0),
+        (0.0, 0.0),
+    ]);
 
-    assert_eq!(points, vec![(0.0, 0.0)])
+    assert_eq!(
+        poly_line.plot().collect::<Vec<_>>(),
+        vec![
+            Point::new(0.0, 0.0),
+            Point::new(1.0, 0.0),
+            Point::new(2.0, 0.0),
+            Point::new(3.0, 0.0),
+            Point::new(3.0, 1.0),
+            Point::new(3.0, 2.0),
+            Point::new(3.0, 3.0),
+            Point::new(2.0, 3.0),
+            Point::new(1.0, 3.0),
+            Point::new(0.0, 3.0),
+            Point::new(0.0, 2.0),
+            Point::new(0.0, 1.0),
+            Point::new(0.0, 0.0)
+        ]
+    )
+}
+
+#[test]
+fn it_draws_a_diamond_accross_all_quadrants() {
+    let poly_line = PolyLine::new(vec![
+        (-2.0, 0.0),
+        (0.0, 2.0),
+        (2.0, 0.0),
+        (0.0, -2.0),
+        (-2.0, 0.0),
+    ]);
+
+    assert_eq!(
+        poly_line.plot().collect::<Vec<_>>(),
+        vec![
+            Point::new(-2.0, 0.0),
+            Point::new(-1.0, 1.0),
+            Point::new(0.0, 2.0),
+            Point::new(1.0, 1.0),
+            Point::new(2.0, 0.0),
+            Point::new(1.0, -1.0),
+            Point::new(0.0, -2.0),
+            Point::new(-1.0, -1.0),
+            Point::new(-2.0, 0.0),
+        ]
+    )
 }
