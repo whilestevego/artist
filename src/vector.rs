ac514a12d5ba20b1e5c96d1a6df312fbc1547fbe
Steve Godin <steve.god@gmail.com>
2018-11-12 16:00:17 -0800

    Document and unit test Vector

--- src/vector.rs
+++ src/vector.rs
@@ -6,10 +6,28 @@ use std::ops::{Add, Sub};
 pub struct Vector(pub f64, pub f64);
 
 impl Vector {
+    /// The scalar porpotion of the vector or its length.
+    ///
+    /// ```
+    /// extern crate graphics;
+    /// use graphics::*;
+    ///
+    /// assert_eq!(Vector(1.0, 1.0).magnitude(), 1.4142135623730951);
+    /// assert_eq!(Vector(3.0, 4.0).magnitude(), 5.0);
+    /// ```
     pub fn magnitude(&self) -> f64 {
         (self.0.powf(2.0) + self.1.powf(2.0)).sqrt()
     }
 
+    /// A unit length vector representing the vector's direction.
+    ///
+    /// ```
+    /// extern crate graphics;
+    /// use graphics::*;
+    ///
+    /// assert_eq!(Vector(1.0, 1.0).direction(), Vector(0.7071067811865475, 0.7071067811865475));
+    /// assert_eq!(Vector(3.0, 4.0).direction(), Vector(0.6, 0.8));
+    /// ```
     pub fn direction(&self) -> Self {
         let magnitude = self.magnitude();
 
@@ -28,6 +46,15 @@ impl Vector {
 impl Add for Vector {
     type Output = Self;
 
+    /// Implements `+` for Vector
+    ///
+    /// ```
+    /// extern crate graphics;
+    /// use graphics::*;
+    ///
+    /// assert_eq!(Vector(1.0, 1.0) + Vector(1.0, 1.0), Vector(2.0, 2.0));
+    /// assert_eq!(Vector(3.0, 4.0) + Vector(7.0, -8.0), Vector(10.0, -4.0));
+    /// ```
     fn add(self, other: Self) -> Self {
         Vector(self.0 + other.0, self.1 + other.1)
     }
@@ -36,6 +63,15 @@ impl Add for Vector {
 impl Sub for Vector {
     type Output = Self;
 
+    /// Implements `-` for Vector
+    ///
+    /// ```
+    /// extern crate graphics;
+    /// use graphics::*;
+    ///
+    /// assert_eq!(Vector(1.0, 1.0) - Vector(1.0, 1.0), Vector(0.0, 0.0));
+    /// assert_eq!(Vector(3.0, 4.0) - Vector(7.0, -8.0), Vector(-4.0, 12.0));
+    /// ```
     fn sub(self, other: Self) -> Self {
         Vector(self.0 - other.0, self.1 - other.1)
     }
@@ -47,12 +83,9 @@ impl Ord for Vector {
     fn cmp(&self, other: &Self) -> Ordering {
         self.0
             .partial_cmp(&other.0)
-            .and_then(|ord| {
-                if ord == Ordering::Equal {
-                    self.1.partial_cmp(&other.1)
-                } else {
-                    Some(ord)
-                }
+            .and_then(|ord| match ord {
+                Ordering::Equal => self.1.partial_cmp(&other.1),
+                _ => Some(ord),
             }).expect("Could not compare values")
     }
 }
