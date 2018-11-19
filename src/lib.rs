dda272fd01f5af43317b5f96ab896cebfe558605
Steve Godin <steve.god@gmail.com>
2018-11-19 08:45:15 -0800

    Add Axis enum

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,13 @@
 extern crate image;
 
+mod axis;
 mod line;
 mod plotable;
 mod poly_line;
 mod renderable;
 mod vector;
 
+pub use self::axis::*;
 pub use self::line::*;
 pub use self::plotable::*;
 pub use self::poly_line::*;
