7e9e6e569dccc8619056536b11a943598174169b
Steve Godin <steve.god@gmail.com>
2018-12-01 14:55:38 -0800

    Use generic Integer for Point instead of macro in some cases

--- src/point.rs
+++ src/point.rs
@@ -4,33 +4,33 @@ use num::Integer;
 #[derive(Clone, Copy, Eq, Debug, Default, Ord, PartialEq, PartialOrd)]
 pub struct Point<T: Integer>(pub T, pub T);
 
+impl<T: Integer + Copy> Point<T> {
+    pub fn get(&self, axis: &Axis) -> T {
+        match axis {
+            Axis::X => self.0,
+            Axis::Y => self.1,
+        }
+    }
+
+    pub fn get_mut(&mut self, axis: &Axis) -> &mut T {
+        match axis {
+            Axis::X => &mut self.0,
+            Axis::Y => &mut self.1,
+        }
+    }
+}
+
+impl<T: Integer> PartialEq<(T, T)> for Point<T> {
+    fn eq(&self, other: &(T, T)) -> bool {
+        self.0.eq(&other.0) && self.1.eq(&other.1)
+    }
+}
+
 macro_rules! impl_point {
     ($T:ty) => {
-        impl Point<$T> {
-            pub fn get(&self, axis: &Axis) -> $T {
-                match axis {
-                    Axis::X => self.0,
-                    Axis::Y => self.1,
-                }
-            }
-
-            pub fn get_mut(&mut self, axis: &Axis) -> &mut $T {
-                match axis {
-                    Axis::X => &mut self.0,
-                    Axis::Y => &mut self.1,
-                }
-            }
-        }
-
         impl From<Vector> for Point<$T> {
             fn from(v: Vector) -> Self {
                 Point(v.0.round() as $T, v.1.round() as $T)
-            }
-        }
-
-        impl PartialEq<($T, $T)> for Point<$T> {
-            fn eq(&self, other: &($T, $T)) -> bool {
-                self.0.eq(&other.0) && self.1.eq(&other.1)
             }
         }
 
